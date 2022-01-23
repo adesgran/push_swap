a2ca98cf3a0567f72592fedfdb9b845baaae1ae3
adesgran <adesgran.dev@gmail.com>
2022-01-23 19:40:39 +0100

    Test

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:00:24 by adesgran          #+#    #+#             */
-/*   Updated: 2022/01/23 16:49:02 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/01/23 19:40:29 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,7 @@ int	main(int ac, char **av)
 	piles = init_piles(ac - 1, av + 1);
 	if (!piles)
 		return (1);
+	
 	print_piles(piles);
 	sort2(piles);
 	free_piles(piles);
