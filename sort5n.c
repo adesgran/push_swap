6d63763487fc43ec80b25765689f59bc3bfcf9ec
Antoine DESGRANGES <adesgran@student.42.fr>
2022-03-28 15:23:58 +0200

    split function  ok (need to reduce line number)

--- sort5n.c
+++ sort5n.c
@@ -6,7 +6,7 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 14:25:04 by adesgran          #+#    #+#             */
-/*   Updated: 2022/03/28 15:07:12 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/03/28 15:23:55 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,41 @@ static int	get_max(int *pile, int init)
 		i++;
 	}
 	return (res);
+}
+
+static	split_piles(t_piles *piles, int *pile, int init)
+{
+	if (pile[init + 2] > pile[init + 3] && pile[init + 2] > pile[init + 4])
+	{
+		if (pile[init + 3] < pile[init + 4])
+			swap_a(piles);
+		push_b(piles);
+		push_b(piles);
+		push_b(piles);
+	}
+	else if (pile[init + 3] > pile[init + 2] && pile[init + 3] > pile[init + 4])
+	{
+		push_b(piles);
+		swap_a(piles);
+		push_b(piles);
+		if (piles->pile_b[piles->len_b - 1] > piles->pile_b[piles->len_b - 2])
+			swap_b(piles);
+		push_b(piles);
+	}
+	else
+	{
+		push_b(piles);
+		if (piles->len_b == 1)
+			rotate_b(piles);
+		if (pile[init + 2] < pile[init + 3])
+			swap_a(piles);
+		push_b(piles);
+		push_b(piles);
+		if (piles->len_b == 3)
+			rrotate_b(piles);
+	}
+	if (pile[init] > pile[init + 1])
+		swap_a(piles);
 }
 
 void	sort5n(t_piles *piles)
