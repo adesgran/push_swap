08729d7dce39085db6bfce4ee45ba8b93ae8bd66
adesgran <adesgran.dev@gmail.com>
2022-03-30 15:03:06 +0200

    Fixed

--- sort5n.c
+++ sort5n.c
@@ -6,26 +6,26 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 14:25:04 by adesgran          #+#    #+#             */
-/*   Updated: 2022/03/28 15:38:00 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/03/30 15:03:03 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-static int	get_max(int *pile, int init)
+static int	get_min(int *pile, int init)
 {
-	int	max;
+	int	min;
 	int	res;
 	int	i;
 
-	max = pile[init];
+	min = pile[init];
 	res = init;
 	i = 1;
 	while (i < 5)
 	{
-		if (pile[init + i] > max)
+		if (pile[init + i] < min)
 		{
-			max = pile[init + i];
+			min = pile[init + i];
 			res = init + i;
 		}
 		i++;
@@ -43,7 +43,7 @@ static void	merge_piles(t_piles *piles, int init)
 	count = 0;
 	while (len_a < 5)
 	{
-		if (piles->pile_b[piles->len_b - 1] > piles->pile_a[init + len_a - 1] || count == len_a)
+		if (piles->pile_b[piles->len_b - 1] < piles->pile_a[init + len_a - 1] || count == len_a)
 		{
 			push_a(piles);
 			count++;
@@ -65,20 +65,20 @@ static void	merge_piles(t_piles *piles, int init)
 
 static void	split_piles(t_piles *piles, int *pile, int init)
 {
-	if (pile[init + 2] > pile[init + 3] && pile[init + 2] > pile[init + 4])
+	if (pile[init + 2] < pile[init + 3] && pile[init + 2] < pile[init + 4])
 	{
-		if (pile[init + 3] < pile[init + 4])
+		if (pile[init + 3] > pile[init + 4])
 			swap_a(piles);
 		push_b(piles);
 		push_b(piles);
 		push_b(piles);
 	}
-	else if (pile[init + 3] > pile[init + 2] && pile[init + 3] > pile[init + 4])
+	else if (pile[init + 3] < pile[init + 2] && pile[init + 3] < pile[init + 4])
 	{
 		push_b(piles);
 		swap_a(piles);
 		push_b(piles);
-		if (piles->pile_b[piles->len_b - 1] > piles->pile_b[piles->len_b - 2])
+		if (piles->pile_b[piles->len_b - 1] < piles->pile_b[piles->len_b - 2])
 			swap_b(piles);
 		push_b(piles);
 	}
@@ -87,15 +87,16 @@ static void	split_piles(t_piles *piles, int *pile, int init)
 		push_b(piles);
 		if (piles->len_b == 1)
 			rotate_b(piles);
-		if (pile[init + 2] < pile[init + 3])
+		if (pile[init + 2] > pile[init + 3])
 			swap_a(piles);
 		push_b(piles);
 		push_b(piles);
 		if (piles->len_b == 3)
 			rrotate_b(piles);
 	}
-	if (pile[init] > pile[init + 1])
+	if (pile[init] < pile[init + 1])
 		swap_a(piles);
+	merge_piles(piles, init);
 }
 
 void	sort5n(t_piles *piles)
@@ -105,8 +106,12 @@ void	sort5n(t_piles *piles)
 
 	pile = piles->pile_a;
 	init = piles->len_a - 5;
-	if (get_max(pile, init) == init + 3)
+	if (get_min(pile, init) == init + 3)
 		swap_a(piles);
-	if (get_max(pile, init) == init + 4)
+	if (get_min(pile, init) == init + 4)
+	{
+		rotate_a(piles);
 		return (sort4n(piles));
+	}
+	split_piles(piles, pile, init);
 }
