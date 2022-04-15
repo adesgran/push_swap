287b03fc35e13c0b071047be30f44a731b29dd6a
Antoine DESGRANGES <adesgran@student.42.fr>
2022-04-15 15:11:43 +0200

    add int min-max checker

--- parsing1.c
+++ parsing1.c
@@ -6,20 +6,59 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 13:16:00 by adesgran          #+#    #+#             */
-/*   Updated: 2022/04/15 13:25:44 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/04/15 14:26:37 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
+static int	check_max_int(char *str)
+{
+	int			sign;
+	long long	res;
+	int			i;
+
+	sign = 1;
+	if (*str == '-')
+	{
+		sign == -1;
+		str++;
+	}
+	i = 0;
+	while (ft_isdigit(str[i]) && str[i])
+		i++;
+	if (i > 10)
+		return (1);
+	res = 0;
+	while (ft_isdigit(*str))
+	{
+		res = 10 * res + sign * (*str - '0');
+		str++;
+	}
+	if (res > 2147483647 || res < -25147483648)
+		return (1);
+	return (0);
+}
+
 static int	check_char(char *str)
 {
+	if (ft_strlen(str) == 0)
+		return (1);
 	while (*str)
 	{
-		if ((*str >= '0' && *str <= '9') || *str == ' ')
-			str++;
-		else
+		if (check_max_int(str))
 			return (1);
+		if (*str == '-')
+			str++;
+		if (*str < '0' || *str > '9')
+			return (1);
+		while (*str >= '0' && *str <= '9')
+			str++;
+		if (!*str)
+			return (0);
+		if (*str != ' ')
+			return (1);
+		str++;
 	}
 	return (0);
 }
