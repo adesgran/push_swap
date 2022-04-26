f2c1fb4875282b945c2fe4366e87b9a4dadd594e
Antoine Desgranges <adesgran.dev@gmail.com>
2022-04-26 16:33:20 +0200

    Norm

--- sort3n.c
+++ sort3n.c
@@ -6,66 +6,43 @@
 /*   By: adesgran <adesgran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 12:45:46 by adesgran          #+#    #+#             */
-/*   Updated: 2022/04/26 16:12:16 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/04/26 16:33:05 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-static void	sequence_1(t_piles *piles)
+void	sort3n_case3(t_piles *piles, int init, int *pile)
 {
-	if (piles->len_a == 3)
-	{
-		rotate_a(piles);
-		return ;
-	}
-	swap_a(piles);
-	rotate_a(piles);
-	swap_a(piles);
-	rotate_a(piles);
-}
-
-static void	sequence_2(t_piles *piles)
-{
-	if (piles->len_a == 3)
-	{
-		rrotate_a(piles);
-		return ;
-	}
 	push_b(piles);
-	push_b(piles);
-	rotate_a(piles);
-	push_a(piles);
-	push_a(piles);
-	rotate_a(piles);
-}
-
-static void	sequence_3(t_piles *piles)
-{
-	if (piles->len_a == 3)
-	{
+	if (pile[init] < pile[init + 1])
 		swap_a(piles);
-		rrotate_a(piles);
-		return ;
-	}
-	push_b(piles);
-	swap_a(piles);
 	rotate_a(piles);
 	rotate_a(piles);
 	push_a(piles);
 }
 
-static void	sequence_4(t_piles *piles)
+void	sort3n_case2(t_piles *piles, int init, int *pile)
 {
-	if (piles->len_a == 3)
+	if (pile[init + 1] > pile[init] && pile[init + 1] > pile[init + 2])
 	{
-		swap_a(piles);
-		rotate_a(piles);
-		return ;
+		if (pile[init + 2] < pile[init])
+		{
+			rotate_a(piles);
+			swap_a(piles);
+			rotate_a(piles);
+		}
+		else
+		{
+			push_b(piles);
+			swap_a(piles);
+			rotate_a(piles);
+			push_a(piles);
+			rotate_a(piles);
+		}
 	}
-	rotate_a(piles);
-	swap_a(piles);
-	rotate_a(piles);
+	else
+		sort3n_case3(piles, init, pile);
 }
 
 void	sort3n(t_piles *piles)
@@ -73,30 +50,18 @@ void	sort3n(t_piles *piles)
 	int	init;
 	int	*pile;
 
-	init = piles->len_a - 3;
 	pile = piles->pile_a;
-	if (pile[init] < pile[init + 2])
+	init = piles->len_a - 3;
+	if (pile[init] > pile[init + 1] && pile[init] > pile[init + 2])
 	{
-		if (pile[init + 1] < pile[init])
-			sequence_1(piles);
-		else if (pile[init + 1] > pile[init + 2])
-			sequence_2(piles);
-		else
-			sequence_3(piles);
-	}
-	else
-	{
-		if (pile[init + 1] > pile[init])
-			sequence_4(piles);
-		else
+		if (pile[init + 1] < pile[init + 2])
+			swap_a(piles);
+		if (init != 0)
 		{
-			if (pile[init + 1] < pile[init + 2])
-				swap_a(piles);
-			if (piles->len_a > 3)
-			{
-				rotate_a(piles);
-				rotate_a(piles);
-			}
+			rotate_a(piles);
+			rotate_a(piles);
 		}
 	}
+	else
+		sort3n_case2(piles, init, pile);
 }
