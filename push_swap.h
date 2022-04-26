1fcac86e72a55f79d631922e7afec41ee09bcb84
Antoine Desgranges <adesgran.dev@gmail.com>
2022-04-26 13:21:47 +0200

    Remove debug definition

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 13:59:13 by adesgran          #+#    #+#             */
-/*   Updated: 2022/04/21 14:22:09 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/04/26 13:21:40 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,14 +55,5 @@ void	sort4n(t_piles *piles);
 void	sort5n(t_piles *piles);
 void	sort5nb(t_piles *piles);
 int		parsing(char *str);
-#ifndef __DEBUG__
-#define __DEBUG__
 
-#ifdef DEBUG
-#define debug(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
-#else
-#define debug(fmt, ...) ((void)0)
-#endif
-
-#endif
 #endif
